0ffcd126fdb362d54a6554d4e749f8da6ad7c5c9
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2021-11-22 23:21:16 -0300

    Cria estrutura review

---
+++ trabalho.cpp
@@ -0,0 +1,18 @@
+#include <fstream>
+#include <string>
+
+typedef struct
+{
+    char id[150];
+    char text[250];
+    int upvotes;
+    char version[10];
+    char timeDate[20];
+
+}Review;
+
+
+int main (){
+
+
+}
